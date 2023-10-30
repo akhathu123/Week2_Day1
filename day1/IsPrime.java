a935fe56ef1100751c505c30ffb05289cbdf96c1
akhathu123 <76878065+akhathu123@users.noreply.github.com>
2023-10-30 18:06:53 +0530

    Week2 Day 1 assessments

---
+++ day1/IsPrime.java
@@ -0,0 +1,21 @@
+package week2.day1;
+
+public class IsPrime {
+
+	public static void main(String[] args) {
+		int num = 3;
+		int count=0;
+		
+		for (int i = 1; i <=num; i++) {
+			if(num%i==0)
+				count ++;
+		}
+		if (count==2) {
+			System.out.println("Given number is Prime");
+		} else {
+			System.out.println("Given number is Not Prime");
+		}
+
+	}
+
+}
